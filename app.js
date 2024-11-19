2a67046cd39ff44890cfc1112e17a3060d451849
XandePedrassani <alexandrecouss@hotmail.com>
2024-11-19 20:45:19 -0300

    Adicionando o projeto base criado em sala de aula

---
+++ app.js
@@ -0,0 +1,32 @@
+const routes = require('./routers/route');
+const handlebars = require('express-handlebars');
+const express = require('express');
+//var cookieParser = require('cookie-parser');
+var session = require('express-session');
+const middlewares = require('./middlewares/middlewares');
+const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:'textosecreto$asdfasdfaswwww',
+        cookie:{maxAge: 30*60*1000}}));
+//app.use(cookieParser());
+
+
+app.engine('handlebars', handlebars.engine({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(middlewares.logRegister,middlewares.sessionControl)
+app.use(routes);
+
+app.use(
+    express.urlencoded({
+      extended: true
+    })
+)
+
+app.listen(8081, function(){
+        console.log("Servidor no http://localhost:8081")
+});
---
+++ routers/route.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const db = require('../config/db_sequelize');
+const controllerUsuario = require('../controllers/controllerUsuario');
+const controllerComentario = require('../controllers/controllerComentario');
+const controllerCategoria = require('../controllers/controllerCategoria');
+const controllerReceita = require('../controllers/controllerReceita');
+const multer = require('multer');
+const route = express.Router();
+
+/*db.sequelize.sync({force: true}).then(() => {
+    console.log('{ force: true }');
+});*/
+//db.Usuario.create({login:'admin', senha:'1234', tipo:2});
+
+
+module.exports = route;
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null,  "public/uploads/");
+    },
+    filename: (req, file, cb) => {
+        req.imageName = req.body.nome + '.png'
+        cb(null, req.imageName)
+    },
+})
+const upload = multer({ storage: storage });
+
+//Home
+route.get("/home", function (req, res) {
+
+    if (req.session.login) {
+        res.render('home')
+    }
+    else
+        res.redirect('/');
+});
+
+//Controller Usuario
+route.get("/", controllerUsuario.getLogin);
+route.post("/login", controllerUsuario.postLogin);
+route.get("/logout", controllerUsuario.getLogout);
+route.get("/usuarioCreate", controllerUsuario.getCreate);
+route.post("/usuarioCreate", controllerUsuario.postCreate);
+route.get("/usuarioList", controllerUsuario.getList);
+route.get("/usuarioUpdate/:id", controllerUsuario.getUpdate);
+route.post("/usuarioUpdate", controllerUsuario.postUpdate);
+route.get("/usuarioDelete/:id", controllerUsuario.getDelete);
+
+//Controller Categoria
+route.get("/categoriaCreate", controllerCategoria.getCreate);
+route.post("/categoriaCreate", controllerCategoria.postCreate);
+route.get("/categoriaList", controllerCategoria.getList);
+route.get("/categoriaUpdate/:id", controllerCategoria.getUpdate);
+route.post("/categoriaUpdate", controllerCategoria.postUpdate);
+route.get("/categoriaDelete/:id", controllerCategoria.getDelete);
+
+//Controller Receita
+route.get("/receitaCreate", controllerReceita.getCreate);
+route.post("/receitaCreate",  upload.single('imagem'), controllerReceita.postCreate);
+route.get("/receitaList", controllerReceita.getList);
+route.get("/receitaUpdate/:id", controllerReceita.getUpdate);
+route.post("/receitaUpdate", upload.single('imagem'), controllerReceita.postUpdate);
+route.get("/receitaDelete/:id", controllerReceita.getDelete);
+
+//Controller Comentario
+route.get("/comentarioCreate", controllerComentario.getCreate);
+route.post("/comentarioCreate", controllerComentario.postCreate);
+route.get("/comentarioList", controllerComentario.getList);
